c77af67c9522048e2e8b8a0c6263c8bdf8d1cb4e
Ilaria <ilaria.orlando@iismajoranamoncalieri.edu.it>
2024-01-08 11:52:23 +0100

    Creata connesssione

---
+++ connessione.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <p align= 'centre'>Connessione al database</strong><br><br><br>
+        <?php
+            $hostname = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "prodotti"
+            //connesssione al server sql con selezione del database
+            $conn = mysqli_connect($hostname, $username, $password, $dbname);
+            if(!$conn)
+                die("<br><br><strong>Errore nella connessione</strong>");
+            else 
+                print"<br><br><strong>Connessione avvenuta correttamente</strong><br><br>";
+            mysqli_close($conn)
+            print"<strong>Ora la connessione è chiusa</strong>";
+        ?>
+    </body>
+</html>
