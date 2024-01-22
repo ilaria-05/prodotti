52deda23162f56cc5e1304b991a629f930c2ccbb
Ilaria <ilaria.orlando@iismajoranmoncalieri.edu.it>
2024-01-22 10:46:23 +0100

    Corretti errotri

--- inserimento_prodotto.php
+++ inserimento_prodotto.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html> 
     <head></head>
-    <body> 
-    <!DOCTYPE html>
-<html> 
-    <head></head>
     <body>
     <h1>Inserimento di un nuovo prodotto</h1>
         <?php
-    
+        session_start();
+     $codice_prodotto = $_POST['codice_prodotto'];
+     $descrizione = $_POST['descrizione'];
+     $prezzo_unitario = $_POST['prezzo_unitario'];
+     $quantita = $_POST['quantita'];
+
             $hostname = "localhost";
             $username = "root";
             $password = "";
@@ -20,18 +21,16 @@
                 exit();
             }
             //recupera i dati passati daL form html
-            $codice_prodotto = $_POST['codice_prodotto'];
-            $descrizione = $_POST['descrizione'];
-            $prezzo_unitario = $_POST['prezzo_unitario'];
-            $quantita = $_POST['quantita'];
-            $query = "INSERT INTO prodotti (codice_prodotto, descrizione, prezzo_unitario, quantita) VALUES 
-            ('$codice_prodotto', '$quantita', $prezzo_unitario, $quantita)";
-            $risultato = mysqli_query($conn,$query);
-            $n_riche = mysqli_affected_rows($conn);
+           
+            $query= "INSERT INTO prodotti(codice_prodotto, descrizione, prezzo_unitario, quantita)VALUES 
+            ('$codice_prodotto', '$descrizione', $prezzo_unitario, $quantita)";
+            $risultato = mysqli_real_query($conn,$query);
             if(!$risultato){
                 print "Errore nell'inserimento";
+                exit();
             } else {
-                print "Registrazine avvanuta correttamente <br> sono state inserite $n_righe righe.";
+                print "Registrazine avvenuta correttamente <br> è stata inserita 1 riga.";
+                exit();
             }
             mysqli_close($conn);
         ?>
