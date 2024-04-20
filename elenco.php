7047f9d0396aa390820c207c0dc8694553393232
Ilaria <145005392+ilaria-05@users.noreply.github.com>
2024-04-20 15:29:52 +0200

    modificato

--- elenco.php
+++ elenco.php
@@ -1,6 +1,41 @@
 <!DOCTYPE html>
 <html>
-    <head></head>
+    <head>
+        <style>
+            body {margin:0;}
+            
+            ul {
+              list-style-type: none;
+              margin: 0;
+              padding: 0;
+              overflow: hidden;
+              background-color: #333;
+              position: fixed;
+              top: 0;
+              width: 100%;
+            }
+            
+            li {
+              float: left;
+            }
+            
+            li a {
+              display: block;
+              color: white;
+              text-align: center;
+              padding: 14px 16px;
+              text-decoration: none;
+            }
+            
+            li a:hover:not(.active) {
+              background-color: #1fcab3;
+            }
+            
+            .active {
+              background-color: #1fcab3;
+            }
+            </style>
+    </head>
     <body>
         <h1 style= "font size='5'">Elenco prodotti</h1>
         <?php
