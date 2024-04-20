be0d1fbe1be83e9e28061a1981cd04ddeec519e7
Ilaria <145005392+ilaria-05@users.noreply.github.com>
2024-04-20 15:34:39 +0200

    modificato

--- codice.php
+++ codice.php
@@ -37,14 +37,12 @@
             </style>
     </head>
     <body>
-        <ul>
-          <li><a href="http://localhost/prodotti/home.html">Homepage</a></li>
-          <li><a href="http://localhost/prodotti/elenco.php">Tabella prodotti</a></li>
-          <li><a href="http://localhost/prodotti/codice_prodotto.html">Inserimento codice prodotto</a></li>
-          <li><a href="http://localhost/prodotti/codice_prodotto.php"> Inserimento nuovo prodotto</a></li> 
-          <li><a href="http://localhost/prodotti/registrati.html">Registrati</a></li>
-          <li><a href="http://localhost/prodotti/accedi.html">Accedi</a></li>
-        </ul><br><br> <br><br>
+         <ul>
+        <li><a href="http://localhost/prodotti/home.html">Homepage</a></li>
+        <li><a href="http://localhost/prodotti/elenco.php">Tabella prodotti</a></li>
+        <li><a href="http://localhost/prodotti/codice_prodotto.html">Inserimento codice prodotto</a></li>
+        <li><a href="http://localhost/prodotti/inserimento_prodotto.php"> Inserimento nuovo prodotto</a></li> 
+          </ul><br><br> <br><br>
     <h1>Elenco dei prodotti:</h1>
       <?php
         $hostname = "localhost";
